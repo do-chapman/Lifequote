0fc47eea9c84a3088ec92dc81aa3f612b4ca57d5
David Chapman <do_chapman@hotmail.com>
2016-09-12 23:03:46 +0100

    Range

--- lifequote/dob.cs
+++ lifequote/dob.cs
@@ -8,9 +8,15 @@ namespace lifequote
 {
     class dob
     {
-        static void Main(string[] args)
-        {
-            ASCIIEncoding
+    public static int LookUpTable< TRange, TValue > (IList < TRange > ranges, TValue value, IRangeComparer< TRange, TValue > comparer)
+    where TRange : Range<TValue> // Specify what you know about TRange and TValue
+    where TValue:
+            IComparable < TValue >
+{
+                int indexToTable = BinarySearch(ranges, value, comparer);
+                TRange lookUp = ranges[indexToTable]; // lookUp is TRange, not Range<TRange>
+                return lookUp.Value;
+            }
         }
     }
 }

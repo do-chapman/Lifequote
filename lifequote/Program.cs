c454fa090fafc9bd99acf9cc4957686b056cf925
David Chapman <do_chapman@hotmail.com>
2016-09-12 16:31:33 +0100

    1st save

---
+++ lifequote/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lifequote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XmlDocument();
+            doc.Load(@"XmlFile.xml");
+
+            var root = doc.DocumentElement;
+
+            if (root == null)
+                return;
+
+            var quotes = root.SelectNodes("quote");
+            if (quotes == null)
+                return;
+
+            foreach (XmlNode quote in quotes)
+            {
+                var title = quote.SelectSingleNode("title");
+                var fname = quote.SelectSingleNode("fname");
+                var lname = quote.SelectSingleNode("lname");
+                var dob = quote.SelectSingleNode("dob");
+                var gender = quote.SelectSingleNode("gender");
+                var smoke = quote.SelectSingleNode("smoke");
+                var postcode =quote.SelectSingleNode("postcode");
+                var children = quote.SelectSingleNode("children");
+                var exercise = quote.SelectSingleNode("exercise");
+
+                //And so on
+            }
+
+
+        }
+    }
+}
---
+++ lifequote/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lifequote")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BGL Group Ltd")]
+[assembly: AssemblyProduct("lifequote")]
+[assembly: AssemblyCopyright("Copyright © BGL Group Ltd 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bfd969bf-65c5-448c-b358-931cfd2bc820")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
